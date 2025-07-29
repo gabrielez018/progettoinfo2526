5420aae7bf5bc5766225de1c11ef0763d364a506
gabrielez018 <gabrielezampis1807@gmail.com>
2025-07-30 00:08:55 +0200

    r

--- Assets/scripts/TreesSpawnerManager.cs
+++ Assets/scripts/TreesSpawnerManager.cs
@@ -2,16 +2,33 @@ using UnityEngine;
 
 public class TreesSpawnerManager : MonoBehaviour
 {
-    [Header("Log Prefabs")]
     [SerializeField] GameObject[] Logs;
+    [SerializeField] float LogHeight;
+    [SerializeField] int LogCount;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+    void GenerateLogAtStart()
+    {
+        //make the initial y of the log spawner to 0 to make on the floor
+        float NextLogToSpawnPosition = 0;
+        for (int i = 0; i < LogCount; i++)
+        {
+            SpawnNewLog();
+        }
+
+    }
+    void SpawnNewLog()
+    {
+        GameObject RandomLogPrefab = Logs[Random.Range(0, Logs.Length)];
         
     }
 }
