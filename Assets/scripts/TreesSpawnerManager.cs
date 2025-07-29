a5302ddb731a86b90bd1cb0f3eaf4dc83b95a348
gabrielez018 <gabrielezampis1807@gmail.com>
2025-07-29 23:49:42 +0200

    g

---
+++ Assets/scripts/TreesSpawnerManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class TreesSpawnerManager : MonoBehaviour
+{
+    [Header("Log Prefabs")]
+    [SerializeField] GameObject[] Logs;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
