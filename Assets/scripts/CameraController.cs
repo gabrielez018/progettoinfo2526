cdf77670093ef6b8796df2bee934b6a8ef6d694d
gabrielez018 <gabrielezampis1807@gmail.com>
2025-08-17 16:03:47 +0200

    d

---
+++ Assets/scripts/CameraController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] GameObject lookAtObject;
+
+    public float rotationSpeed = 5.0f;
+
+    public float distanceFromTarget = 10.0f;
+    //y camera distance the lookatobject
+    public float height = 5.0f;
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+
+    [SerializeField] float minClamp = -60f;
+    [SerializeField] float maxClamp = 60f;
+
+    void Update()
+    {
+        currentX += Input.GetAxis("Mouse X") * rotationSpeed;
+        currentY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        
+        currentY = Mathf.Clamp(currentY, minClamp, maxClamp);
+
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+
+        Vector3 newCameraPosition = rotation * new Vector3(0.0f, height, -distanceFromTarget) + lookAtObject.transform.position;
+
+        // Imposta la posizione e la rotazione della telecamera
+        transform.position = newCameraPosition;
+        transform.LookAt(lookAtObject.transform);
+    }
+}
